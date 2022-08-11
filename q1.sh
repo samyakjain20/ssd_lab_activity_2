a22b302315619a57ebf9bccd58889cd0fc1cac41
samyakjain20 <samyakjain971@gmail.com>
2022-08-11 16:36:00 +0530

    final commit

--- q1.sh
+++ q1.sh
@@ -3,5 +3,6 @@
 lines=$(cat $1 | wc -l)
 mid=$((lines/2 + lines%2))
 
-midLine=$(sed -n "$mid,${mid}p; 8q" $1)
+#midLine=$(sed -n "$mid,${mid}p; 8q" $1)
+midLine=$(awk -v mid=$mid '{if(NR==mid) print $0}' $1)
 echo $midLine
--- q2.sh
+++ q2.sh
@@ -1,5 +1,4 @@
 #! /bin/bash
-
 #cat $1
 
 awk -F/ '{if( $2=="usr" ) print $0}' /etc/shells | cut -d "/" -f 4
